936504a9576bb3d4bfa24fd9ba2c3de9e3ccb0d1
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-16 14:52:07 -0600

    add return value to task

--- src/actions/example-task.ts
+++ src/actions/example-task.ts
@@ -1,4 +1,5 @@
 import { NewTaskActionFunction } from "hardhat/types/tasks";
+import { ExamplePluginConfig } from "../types.js";
 
 export interface ExampleTaskActionArguments {
   quiet: boolean;
@@ -7,13 +8,15 @@ export interface ExampleTaskActionArguments {
 const action: NewTaskActionFunction<ExampleTaskActionArguments> = async (
   args,
   hre,
-) => {
+): Promise<ExamplePluginConfig> => {
+  const config = hre.config.examplePlugin;
+  
   if (!args.quiet) {
-    const config = hre.config.examplePlugin;
-
     console.log("Example Plugin Config:");
     console.log(config);
   }
+
+  return config;
 };
 
 export default action;
