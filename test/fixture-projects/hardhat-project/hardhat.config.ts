decefeb9865fb0b59d5e950831af64fc9f9dbb2c
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-16 14:23:33 -0600

    replace mocha and chai packages with hardhat-node-test-runner

--- test/fixture-projects/hardhat-project/hardhat.config.ts
+++ test/fixture-projects/hardhat-project/hardhat.config.ts
@@ -3,9 +3,10 @@ import { HardhatUserConfig } from "hardhat/types/config";
 // We load the plugin here.  It must be registered in the `plugins` array of
 // the HardhatUserConfig to have effect.
 import HardhatExamplePlugin from "../../../src/index.js";
+import HardhatNodeTestRunner from '@nomicfoundation/hardhat-node-test-runner';
 
 const config: HardhatUserConfig = {
-  plugins: [HardhatExamplePlugin],
+  plugins: [HardhatNodeTestRunner, HardhatExamplePlugin],
   examplePlugin: { value: "asdf" },
 };
 
