1500b97cdc57b74aa18c1c1c30ae72405281410b
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-16 14:06:54 -0600

    comment unused runtime extension code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
-import { extendEnvironment } from "hardhat/config";
-import { lazyObject } from "hardhat/plugins";
-import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
-import path from "path";
-
-import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
 // This import is needed to let the TypeScript compiler know that it should include your type
 // extensions in your npm package's types file.
 import "./type-extensions";
-
-extendEnvironment((hre) => {
-  // We add a field to the Hardhat Runtime Environment here.
-  // We use lazyObject to avoid initializing things until they are actually
-  // needed.
-  hre.example = lazyObject(() => new ExampleHardhatRuntimeEnvironmentField());
-});
 
 import type { HardhatPlugin } from "hardhat/types/plugins";
 import pkg from "../package.json";
@@ -33,3 +20,15 @@ const plugin: HardhatPlugin = {
 
 // The HardhatPlugin must be exported so that users can register it in their config.
 export default plugin;
+
+// import { extendEnvironment } from "hardhat/config";
+// import { lazyObject } from "hardhat/plugins";
+
+// import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
+
+// extendEnvironment((hre) => {
+//   // We add a field to the Hardhat Runtime Environment here.
+//   // We use lazyObject to avoid initializing things until they are actually
+//   // needed.
+//   hre.example = lazyObject(() => new ExampleHardhatRuntimeEnvironmentField());
+// });
--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -2,9 +2,9 @@
 
 // To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
 import "hardhat/types/config";
-import "hardhat/types/runtime";
+// import "hardhat/types/runtime";
 
-import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
+// import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
 
 import type { ExamplePluginUserConfig, ExamplePluginConfig } from "./types.js";
 
@@ -29,10 +29,10 @@ declare module "hardhat/types/config" {
   }
 }
 
-declare module "hardhat/types/runtime" {
-  // This is an example of an extension to the Hardhat Runtime Environment.
-  // This new field will be available in tasks' actions, scripts, and tests.
-  export interface HardhatRuntimeEnvironment {
-    example: ExampleHardhatRuntimeEnvironmentField;
-  }
-}
+// declare module "hardhat/types/runtime" {
+//   // This is an example of an extension to the Hardhat Runtime Environment.
+//   // This new field will be available in tasks' actions, scripts, and tests.
+//   export interface HardhatRuntimeEnvironment {
+//     example: ExampleHardhatRuntimeEnvironmentField;
+//   }
+// }
